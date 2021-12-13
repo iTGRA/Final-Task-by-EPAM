7146fc120e35b4275cb1a6d18eee57bb7ff06334
Anton <github@prilutsky.ru>
2021-12-13 23:52:25 +0400

    first edition

--- postgres.py
+++ postgres.py
@@ -39,7 +39,7 @@ head_html = '''
 <meta content="text/html; charset=UTF-8" http-equiv="content-type">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <title>Final Task for EPAM School © iTGRA</title></head>
-<style>table, th, td {border:1px solid black;}</style>
+<style>table, th, td {border:1px index black;}</style>
 <body>
 <center>
 <br><h3>Here are the following articles in our magazines</h3><br>

8bf1518655fb822a430ba477cf0e30f4b1174793
Anton <92601974+iTGRA@users.noreply.github.com>
2021-12-14 13:44:50 +0400

    Update sql_backup.sh

--- sql_backup.sh
+++ sql_backup.sh
@@ -4,10 +4,9 @@ max_count=3
 db_name=articles
 files=/local/files
 backup=/local/backups
-
 query='SELECT magazines.name, article_types.type, author.author FROM magazines, article_types, author, articles WHERE articles.magazines_id=magazines.id AND articles.article_type_id=article_types.id AND articles.author_id=author.id;'
 
-# shellcheck disable=SC2046
+# Fetching data from SQL into text file.
 echo $query | psql -U anton > $files/$db_name.`date +%F`.sql && echo "Export raw data from table '${db_name}' into .sql file"
 ls -l $files
 
@@ -17,9 +16,8 @@ if [ ${num} -gt ${max_count} ]
 then
    gzip -9 $files/*.sql
    mv $files/*.gz $backup
-   echo "Files were moved into ${backup} directory"
+   echo "${num} files were moved into ${backup} directory"
    ls -l $backup
 else
-   echo "There are no files were moved into ${backup} directory"
+   echo "No files were moved into ${backup} directory"
 fi
-

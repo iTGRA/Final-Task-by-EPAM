7b7a111b43259ffa1355d85d06faede5b71cad52
Anton <92601974+iTGRA@users.noreply.github.com>
2022-01-17 14:44:28 +0400

    Update lvm&mdraid.sh

--- lvm&mdraid.sh
+++ lvm&mdraid.sh
@@ -3,7 +3,9 @@
 pvcreate sda, sdc
 vgcreate vg_1 /sda /sdc
 lvcreate -l 100%FREE -n lv_1 vg_1
-mkfs && mount
+mkfs.ext4 /dev/vg_1/lv_1
+mount /dev/vg_1/lv_1 /local/files
 
 mdadm --create -v /dev/md0 --level=1 --raid-device=2 /dev/sde /dev/sdd
-mkfs && mount
+mkfs.ext4 /dev/md0
+mount /dev/md0 /local/backups

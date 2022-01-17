62460914dced7f47dfdde58346eb667f81a491ef
Anton <92601974+iTGRA@users.noreply.github.com>
2022-01-17 17:03:31 +0400

    Update sql_backup.sh

--- sql_backup.sh
+++ sql_backup.sh
@@ -6,9 +6,10 @@ db_name=articles
 files=/local/files
 backup=/local/backups
 query='SELECT magazines.name, article_types.type, author.author FROM magazines, article_types, author, articles WHERE articles.magazines_id=magazines.id AND articles.article_type_id=article_types.id AND articles.author_id=author.id;'
+db_user=anton
 
 # Fetching data from SQL into text file.
-echo $query | psql -U anton > $files/$db_name.`date +%F`.sql && echo "Export raw data from table '${db_name}' into .sql file"
+echo $query | psql -U $db_user > $files/$db_name.`date +%Y.%m.%d-%H.%M.%S`.sql && echo "Export raw data from table '${db_name}' into .sql file"
 ls -l $files
 
 num=$(ls -l ${files} | wc -l)
